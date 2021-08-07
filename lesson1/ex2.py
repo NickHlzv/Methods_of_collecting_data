024e2e1009bc7e03a9b8d5353c07ea94331e36cb
NickHlzv <nikitos.halzev@gmail.com>
2021-08-07 15:32:56 +0300

    ex2 done

--- lesson1/ex2.py
+++ lesson1/ex2.py
@@ -1,5 +1,6 @@
 import requests
 import json
+
 API_KEY = 'fdb2abab53832eb8205a102170c5f034'
 USER_AGENT = 'wunderkind777'
 headers = {
@@ -15,10 +16,12 @@ payload = {
 r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
 print(r.status_code)
 
+
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
+
 
 jprint(r.json())
 
